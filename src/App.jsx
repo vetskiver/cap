23c943c1957e6ba2ec19c6602ba3751bba574c29
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-03 13:59:59 +0100

    able to fill in the form with inputs, and see your sample query string change in real time, and when submitting query, see the resulting image below

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import APIForm from './components/APIForm';
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
@@ -12,23 +12,111 @@ function App() {
     width: "",
     height: "",
   });
+  const [currentImage, setCurrentImage] = useState(null);
+
   const submitForm = () => {
-  }
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+    if (inputs.url === "" || inputs.url === " ") {
+      alert("You forgot to submit a URL!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value === "") {
+          inputs[key] = defaultValues[key];
+        }
+      }
+      makeQuery();
+    }
+  };
+  
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+    APICall(query).catch(console.error);
+  };
+
+  const APICall = async (query) => {
+    try {
+      const response = await fetch(query);
+      const json = await response.json();
+      if (json.url == null) {
+        alert("Oops! Something went wrong with that query, let's try again");
+      } else {
+        setCurrentImage(json.url);
+        reset();
+      }
+    } catch (error) {
+      console.error("API call failed: ", error);
+    }
+  };
+
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
+  };
+
   return (
     <div className='whole-page'>
       <h1>Build Your Own Screenshot! 📸</h1>
       <APIForm
         inputs={inputs}
-        handleChange = {(e) =>
+        handleChange={(e) =>
           setInputs((prevState) => ({
-            ...prevState, [e.target.name]: e.target.value.trim(), 
-        }))
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
         }
         onSubmit={submitForm}
       />
-      <br></br>
+      <br />
+
+      {/* Conditional rendering for currentImage */}
+      {currentImage ? (
+        <img
+          className="screenshot"
+          src={currentImage}
+          alt="Screenshot returned"
+        />
+      ) : (
+        <div> </div>
+      )}
+
+      {/* Displaying the mock query string */}
+      <div className="container">
+        <h3> Current Query Status: </h3>
+        <p>
+          https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY    
+          <br />
+          &url={inputs.url} <br />
+          &format={inputs.format} <br />
+          &width={inputs.width}
+          <br />
+          &height={inputs.height}
+          <br />
+          &no_cookie_banners={inputs.no_cookie_banners}
+          <br />
+          &no_ads={inputs.no_ads}
+          <br />
+        </p>
+      </div>
+      <br />
     </div>
   );
 }
 
-export default App
+export default App;
