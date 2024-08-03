4c3def73e760428401e338b0ec9b684b3df7c093
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-03 13:38:44 +0100

    created a home page with a text box for each input that I  would like users to adjust and a little info about each input below it's respective text box.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import APIForm from './components/APIForm';
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+  const submitForm = () => {
+  }
+  return (
+    <div className='whole-page'>
+      <h1>Build Your Own Screenshot! 📸</h1>
+      <APIForm
+        inputs={inputs}
+        handleChange = {(e) =>
+          setInputs((prevState) => ({
+            ...prevState, [e.target.name]: e.target.value.trim(), 
+        }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+    </div>
+  );
+}
+
+export default App
