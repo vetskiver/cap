4b56e731c361f6887efd5ae6e0048af2acc9839a
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-03 14:22:25 +0100

    for now the code I have but the screenshot feature still not implemented successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 import APIForm from './components/APIForm';
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
@@ -12,8 +12,6 @@ function App() {
     width: "",
     height: "",
   });
-  const [currentImage, setCurrentImage] = useState(null);
-
   const submitForm = () => {
     let defaultValues = {
       format: "jpeg",
@@ -22,17 +20,17 @@ function App() {
       width: "1920",
       height: "1080",
     };
-    if (inputs.url === "" || inputs.url === " ") {
-      alert("You forgot to submit a URL!");
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!")
     } else {
       for (const [key, value] of Object.entries(inputs)) {
-        if (value === "") {
-          inputs[key] = defaultValues[key];
+        if (value == "") {
+          inputs[key] = defaultValues[key]
         }
       }
-      makeQuery();
     }
-  };
+    makeQuery();
+  }
   
   const makeQuery = () => {
     let wait_until = "network_idle";
@@ -41,21 +39,20 @@ function App() {
     let url_starter = "https://";
     let fullURL = url_starter + inputs.url;
     let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
-    APICall(query).catch(console.error);
+    callAPI(query).catch(console.error);
   };
 
-  const APICall = async (query) => {
-    try {
-      const response = await fetch(query);
-      const json = await response.json();
-      if (json.url == null) {
-        alert("Oops! Something went wrong with that query, let's try again");
-      } else {
-        setCurrentImage(json.url);
-        reset();
-      }
-    } catch (error) {
-      console.error("API call failed: ", error);
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+        }
+    else {
+      setCurrentImage(json.url);
+      reset();
     }
   };
 
@@ -68,55 +65,50 @@ function App() {
       width: "",
       height: "",
     });
-  };
+  }
 
   return (
     <div className='whole-page'>
       <h1>Build Your Own Screenshot! 📸</h1>
       <APIForm
         inputs={inputs}
-        handleChange={(e) =>
+        handleChange = {(e) =>
           setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value.trim(),
-          }))
+            ...prevState, [e.target.name]: e.target.value.trim(), 
+        }))
         }
         onSubmit={submitForm}
-      />
-      <br />
-
-      {/* Conditional rendering for currentImage */}
-      {currentImage ? (
-        <img
-          className="screenshot"
-          src={currentImage}
-          alt="Screenshot returned"
         />
-      ) : (
-        <div> </div>
-      )}
-
-      {/* Displaying the mock query string */}
-      <div className="container">
+        {currentImage ? (
+          <img
+            className="screenshot"
+            src={currentImage}
+            alt="Screenshot returned"
+          />
+        ) : (
+          <div> </div>
+        )}
+        <div className="container">
         <h3> Current Query Status: </h3>
         <p>
           https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY    
-          <br />
-          &url={inputs.url} <br />
-          &format={inputs.format} <br />
+          <br></br>
+          &url={inputs.url} <br></br>
+          &format={inputs.format} <br></br>
           &width={inputs.width}
-          <br />
+          <br></br>
           &height={inputs.height}
-          <br />
+          <br></br>
           &no_cookie_banners={inputs.no_cookie_banners}
-          <br />
+          <br></br>
           &no_ads={inputs.no_ads}
-          <br />
+          <br></br>
         </p>
-      </div>
-      <br />
+        </div>
+
+        <br></br>
     </div>
   );
 }
 
-export default App;
+export default App
